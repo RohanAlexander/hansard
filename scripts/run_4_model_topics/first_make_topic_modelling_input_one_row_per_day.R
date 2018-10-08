f9ffef34abad4f57575bd07a2258de4c671ce886
Rohan Alexander <RohanAlexander@users.noreply.github.com>
2018-10-08 11:02:37 -0400

    Add multiword concatenation

--- scripts/run_4_model_topics/first_make_topic_modelling_input_one_row_per_day.R
+++ scripts/run_4_model_topics/first_make_topic_modelling_input_one_row_per_day.R
@@ -2,7 +2,7 @@
 # Purpose: This file takes Australian Hansard csv files and it converts them to a tidied tibble of text that can be analysed. The words are lowered and punctuation is removed, but stop words are not removed and they are not stemmed here.
 # Author: Rohan Alexander
 # Email: rohan.alexander@anu.edu.au
-# Last updated: 16 September 2018
+# Last updated: 8 October 2018
 # Prerequisites: You need a folder of csv files e.g. the output of parse_each_pdf_and_save_csv_pdftools.R 
 # To do:
 # - 
@@ -12,16 +12,20 @@
 library(furrr)
 library(hunspell)
 library(lubridate)
+library(stringi)
 library(tictoc)
 library(tidytext)
 library(tidyverse)
 # update.packages()
 
+# Read in multiword expression concatenation list e.g. new zealand to new_zealand
+multiword_expressions <- read_csv2("inputs/misc/multiwords_with_corrections.csv", col_types = cols())
+
 
 #### Create the lists of CSV filenames to read in ####
 # Change the path as required:
-# use_this_path_to_get_csvs  <- "outputs/hansard/hansard_csv_files"
-use_this_path_to_get_csvs  <- "/Volumes/Backup/hansard_csv"
+use_this_path_to_get_csvs  <- "outputs/hansard/hansard_csv_files" # For testing
+# use_this_path_to_get_csvs  <- "/Volumes/Backup/hansard_csv"
 
 file_names <-
   list.files(
@@ -58,6 +62,15 @@ tidy_the_hansard_csv_files <-
     # Remove change all to lower
     csv_file_as_parsed_PDF$text <- str_to_lower(csv_file_as_parsed_PDF$text)
     
+    # Combine multiword expressions by _
+    csv_file_as_parsed_PDF$text <-
+      stri_replace_all_regex(
+        csv_file_as_parsed_PDF$text,
+        "\\b" %s+% multiword_expressions$original %s+% "\\b",
+        multiword_expressions$corrected,
+        vectorize_all = FALSE
+      )
+
     # Convert theText column to just one word per row in a column called word
     words_from_text_file <- csv_file_as_parsed_PDF %>%
       select(text) %>% 
