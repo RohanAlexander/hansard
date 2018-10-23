c6b25e5f61e6ff3707040d19f287d018347f24e0
Rohan Alexander <RohanAlexander@users.noreply.github.com>
2018-10-23 20:32:38 +1100

    Update slides

--- scripts/run_6_make_graphs/make_graph_of_topics_example_paper.R
+++ scripts/run_6_make_graphs/make_graph_of_topics_example_paper.R
@@ -3,14 +3,14 @@
 library(lubridate)
 library(tidyverse)
 
-td_gamma <- read_csv("outputs/topic_models_and_gammas/gammas_model_80.csv")
-dates <- read_csv("outputs/topic_models_and_gammas/dates.csv")
+td_gamma <- read_csv("outputs/results-topic_models_and_gammas/gammas_model_60.csv")
+dates <- read_csv("outputs/results-topic_models_and_gammas/dates.csv")
 
 head(td_gamma)
 
 # td_gamma$document %>% unique() %>% length()
 
-td_gamma$date <- rep(dates$dates, 80)
+td_gamma$date <- rep(dates$dates, 60)
 
 set.seed(12345)
 td_gamma <- td_gamma %>% 
@@ -34,3 +34,34 @@ ggsave(
      width = 8,
      units = "in"
   )
+
+
+
+td_gamma_reduced <- td_gamma %>% 
+  filter(topic %in% c(6, 24, 44)) 
+
+ggplot(data = td_gamma, aes(x = document, y = gamma, color = as.factor(topic))) +
+  geom_point(color = "grey", alpha = 0.5) +
+  geom_point(data = td_gamma_reduced, size = 4) +
+  theme_classic() +
+  labs(x = "Date",
+       y = "Gamma") +
+  theme(legend.position="none") +
+  # facet_wrap(vars(topic)) +
+  scale_colour_viridis_d() + 
+  theme(text = element_text(size = 20)) +
+  theme(
+    panel.background = element_rect(fill = "transparent") # bg of the panel
+    , plot.background = element_rect(fill = "transparent", color = NA) # bg of the plot
+    , panel.grid.major = element_blank() # get rid of major grid
+    , panel.grid.minor = element_blank() # get rid of minor grid
+    , legend.background = element_rect(fill = "transparent") # get rid of legend bg
+    , legend.box.background = element_rect(fill = "transparent") # get rid of legend panel bg
+  )
+
+ggsave(
+  "outputs/figures/topics_example-slides.pdf",
+  height = 6,
+  width = 8,
+  units = "in"
+)
--- scripts/run_6_make_graphs/make_graphs_of_mu_election.R
+++ scripts/run_6_make_graphs/make_graphs_of_mu_election.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-update.packages()
+# update.packages()
 
 mu_election <- read_csv("scripts/from_monica/results/mu_election.csv")
 
@@ -53,6 +53,8 @@ ggsave(
   units = "in"
 )
 
+
+
 all_differerences <- mu_election %>% 
   select(election, topic, median) %>% 
   mutate(previousElection = lag(median, 40)) %>% 
@@ -89,3 +91,184 @@ ggsave(
 # draft("myslides.Rmd", template="metropolis", package="binb", edit=FALSE)
 # setwd("myslides")  ## template creates a new subdir
 # render("myslides.Rmd")
+
+
+
+all_differerences <- mu_election %>% 
+  select(election, topic, median) %>% 
+  mutate(previousElection = lag(median, 40)) %>% 
+  mutate(diff = abs(median - previousElection))
+
+slides_data <- mu_election %>% 
+  select(election, topic, median) %>% 
+  mutate(previousElection = lag(median, 40)) %>% 
+  mutate(diff = abs(median - previousElection)) %>% 
+  mutate(grouper = case_when(election %in% c(26) ~ "Menzies-Holt",
+                             election %in% c(29) ~ "Whitlam",
+                             election %in% c(39, 40, 41) ~ "Howard",
+                             TRUE ~ "Other")) %>% 
+  filter(election %in% c(26, 29, 39, 40, 41))
+
+slides_data %>% 
+  filter(election %in% c(26)) %>% 
+  ggplot(aes(x = topic, y = diff, color = as.factor(election))) +
+  geom_point(data = all_differerences, aes(x = topic, y = diff), color = "grey90") +
+  geom_point() +
+  labs(y = "Absolute difference with previous", 
+       x = "Topic") +
+  scale_color_viridis_d(name = "Election") +
+  theme_minimal()
+
+ggsave(
+  "outputs/figures/mu_election-differences-menziesholt.pdf",
+  height = 8,
+  width = 10,
+  units = "in"
+)
+
+
+
+slides_data %>% 
+  filter(election %in% c(29)) %>% 
+  ggplot(aes(x = topic, y = diff, color = as.factor(election))) +
+  geom_point(data = all_differerences, aes(x = topic, y = diff), color = "grey90") +
+  geom_point() +
+  labs(y = "Absolute difference with previous", 
+       x = "Topic") +
+  scale_color_viridis_d(name = "Election") +
+  theme_minimal()
+
+ggsave(
+  "outputs/figures/mu_election-differences-whitlam.pdf",
+  height = 8,
+  width = 10,
+  units = "in"
+)
+
+
+
+slides_data %>% 
+  filter(election %in% c(26)) %>% 
+  ggplot(aes(x = topic, y = diff, color = as.factor(election))) +
+  geom_point(data = all_differerences, aes(x = topic, y = diff), color = "grey90") +
+  geom_point() +
+  labs(y = "Absolute difference with previous", 
+       x = "Topic") +
+  scale_color_viridis_d(name = "Election") +
+  theme_minimal()
+
+ggsave(
+  "outputs/figures/mu_election-differences-menziesholt.pdf",
+  height = 8,
+  width = 10,
+  units = "in"
+)
+
+
+
+slides_data %>% 
+  filter(election %in% c(39, 40, 41)) %>% 
+  ggplot(aes(x = topic, y = diff, color = as.factor(election))) +
+  geom_point(data = all_differerences, aes(x = topic, y = diff), color = "grey90") +
+  geom_point() +
+  labs(y = "Absolute difference with previous", 
+       x = "Topic") +
+  scale_color_viridis_d(name = "Election") +
+  theme_minimal()
+
+ggsave(
+  "outputs/figures/mu_election-differences-howard.pdf",
+  height = 8,
+  width = 10,
+  units = "in"
+)
+
+
+
+
+
+slides_data %>% 
+  filter(election %in% c(29)) %>% 
+  ggplot(aes(x = topic, y = diff, color = as.factor(election))) +
+  geom_point(data = all_differerences, aes(x = topic, y = diff), color = "grey90") +
+  geom_point(size = 4) +
+  labs(y = "Absolute difference with previous", 
+       x = "Topic") +
+  scale_color_viridis_d(name = "Election") +
+  theme_minimal() +
+  theme(text = element_text(size = 20)) +
+  theme(
+    panel.background = element_rect(fill = "transparent") # bg of the panel
+    , plot.background = element_rect(fill = "transparent", color = NA) # bg of the plot
+    , panel.grid.major = element_blank() # get rid of major grid
+    , panel.grid.minor = element_blank() # get rid of minor grid
+    , legend.background = element_rect(fill = "transparent") # get rid of legend bg
+    , legend.box.background = element_rect(fill = "transparent") # get rid of legend panel bg
+  )
+
+
+ggsave(
+  "outputs/figures/mu_election-differences-whitlam-slides.pdf",
+  height = 8,
+  width = 10,
+  units = "in"
+)
+
+
+
+slides_data %>% 
+  filter(election %in% c(26)) %>% 
+  ggplot(aes(x = topic, y = diff, color = as.factor(election))) +
+  geom_point(data = all_differerences, aes(x = topic, y = diff), color = "grey90") +
+  geom_point(size = 4) +
+  labs(y = "Absolute difference with previous", 
+       x = "Topic") +
+  scale_color_viridis_d(name = "Election") +
+  theme_minimal() +
+  theme(text = element_text(size = 20)) +
+  theme(
+    panel.background = element_rect(fill = "transparent") # bg of the panel
+    , plot.background = element_rect(fill = "transparent", color = NA) # bg of the plot
+    , panel.grid.major = element_blank() # get rid of major grid
+    , panel.grid.minor = element_blank() # get rid of minor grid
+    , legend.background = element_rect(fill = "transparent") # get rid of legend bg
+    , legend.box.background = element_rect(fill = "transparent") # get rid of legend panel bg
+  )
+
+
+ggsave(
+  "outputs/figures/mu_election-differences-menziesholt-slides.pdf",
+  height = 8,
+  width = 10,
+  units = "in"
+)
+
+
+
+slides_data %>% 
+  filter(election %in% c(39, 40, 41)) %>% 
+  ggplot(aes(x = topic, y = diff, color = as.factor(election))) +
+  geom_point(data = all_differerences, aes(x = topic, y = diff), color = "grey90") +
+  geom_point(size = 4) +
+  labs(y = "Absolute difference with previous", 
+       x = "Topic") +
+  scale_color_viridis_d(name = "Election") +
+  theme_minimal() +
+  theme(text = element_text(size = 20)) +
+  theme(
+    panel.background = element_rect(fill = "transparent") # bg of the panel
+    , plot.background = element_rect(fill = "transparent", color = NA) # bg of the plot
+    , panel.grid.major = element_blank() # get rid of major grid
+    , panel.grid.minor = element_blank() # get rid of minor grid
+    , legend.background = element_rect(fill = "transparent") # get rid of legend bg
+    , legend.box.background = element_rect(fill = "transparent") # get rid of legend panel bg
+  )
+
+
+ggsave(
+  "outputs/figures/mu_election-differences-howard-slides.pdf",
+  height = 8,
+  width = 10,
+  units = "in"
+)
+
