de10519c6b1868cf39e8271679bfce415ae013c9
Rohan Alexander <RohanAlexander@users.noreply.github.com>
2018-12-15 18:51:16 +0100

    SECOND DRAFT DONE

--- scripts/run_6_make_graphs/make_graph_of_topics-economics.R
+++ scripts/run_6_make_graphs/make_graph_of_topics-economics.R
@@ -39,8 +39,17 @@ td_gamma_reduced <- td_gamma_reduced %>%
 labels <- tibble(
   date = c(ymd("1929-10-29"), ymd("1931-06-11")),
   label_with_this = c("Black Tuesday", "Premiers' Plan"),
-  height = c(0.65, 0.60)
+  height = c(0.60, 0.55)
 )
+
+
+pms <- tibble(
+  date = c(ymd("1929-10-22", "1932-01-06")),
+  label_with_this = c("Scullin", "Lyons"),
+  height = c(0.65, 0.65)
+)
+
+
 
 
 ggplot(data = td_gamma_reduced, 
@@ -51,6 +60,8 @@ ggplot(data = td_gamma_reduced,
   geom_jitter(size = 0.5) +
   geom_smooth() +
   geom_vline(data = labels, aes(xintercept = date), linetype = "dashed") +
+  geom_vline(data = pms, aes(xintercept = date), linetype = "dotted") +
+  geom_label(data = pms, aes(x = date, y = height, label = label_with_this, color = "black"), color = "black") +
   geom_label(data = labels, aes(x = date, y = height, label = label_with_this, color = "black"), color = "black") +
   theme_classic() +
   labs(x = "Date",
@@ -70,51 +81,6 @@ ggsave(
 
 
 
-# Financial criss
-
-# For paper
-td_gamma_reduced <- td_gamma %>% 
-  filter(topic %in% c(17, 22, 25, 44, 51, 55, 59, 71, 72, 74, 76, 77)) 
-# %>% 
-#   filter(topic %in% c(51, 55, 59))
-
-td_gamma_reduced <- td_gamma_reduced %>% 
-  filter(date >= "2007-01-01") %>% 
-  filter(date <= "2010-12-31") 
-
-labels <- tibble(
-  date = c(ymd("2008-09-15")),
-  label_with_this = c("Lehman Brothers"),
-  height = c(0.60)
-)
-
-
-
-ggplot(data = td_gamma_reduced, 
-       aes(x = date, 
-           y = gamma, 
-           color = as.factor(topic))
-) +
-  geom_jitter(size = 0.5) +
-  geom_smooth() +
-  geom_vline(data = labels, aes(xintercept = date), linetype = "dashed") +
-  geom_label(data = labels, aes(x = date, y = height, label = label_with_this, color = "black"), color = "black") +
-  theme_classic() +
-  labs(x = "Date",
-       y = "Proportion") +
-  facet_wrap(vars(chamber), ncol = 2) +
-  # theme(legend.position="none") +
-  scale_colour_viridis_d(name = "Topics")
-
-ggsave(
-  "outputs/figures/topics_example-GFC.pdf",
-  height = 6,
-  width = 8,
-  units = "in"
-)
-
-
-
 
 # 80s
 
@@ -126,12 +92,18 @@ td_gamma_reduced <- td_gamma %>%
 
 td_gamma_reduced <- td_gamma_reduced %>% 
   filter(date >= "1981-01-01") %>% 
-  filter(date <= "2000-12-31") 
+  filter(date <= "1998-12-31") 
 
 labels <- tibble(
   date = c(ymd("1983-12-12", "1987-10-20")),
   label_with_this = c("Float dollar", "Black Tuesday"),
-  height = c(0.50, 0.45)
+  height = c(0.45, 0.40)
+)
+
+pms <- tibble(
+  date = c(ymd("1983-03-11", "1991-12-20", "1996-03-11")),
+  label_with_this = c("Hawke", "Keating", "Howard"),
+  height = c(0.50, 0.5, 0.5)
 )
 
 
@@ -143,7 +115,9 @@ ggplot(data = td_gamma_reduced,
   geom_jitter(size = 0.5) +
   geom_smooth() +
   geom_vline(data = labels, aes(xintercept = date), linetype = "dashed") +
+  geom_vline(data = pms, aes(xintercept = date), linetype = "dotted") +
   geom_label(data = labels, aes(x = date, y = height, label = label_with_this, color = "black"), color = "black") +
+  geom_label(data = pms, aes(x = date, y = height, label = label_with_this, color = "black"), color = "black") +
   theme_classic() +
   labs(x = "Date",
        y = "Proportion") +
@@ -167,32 +141,56 @@ ggsave(
 
 
 
-# For slides
-td_gamma_reduced <- td_gamma %>% 
-  filter(topic %in% c(4, 26, 28, 30, 66))
 
-ggplot(data = td_gamma, aes(x = date, y = gamma, color = as.factor(topic))) +
-  geom_point(color = "grey", alpha = 0.5) +
-  geom_point(data = td_gamma_reduced, size = 4) +
+
+
+# Financial criss
+
+
+td_gamma_reduced <- td_gamma %>% 
+  filter(topic %in% c(17, 22, 25, 44, 51, 55, 59, 71, 72, 74, 76, 77)) 
+# %>% 
+#   filter(topic %in% c(51, 55, 59))
+
+td_gamma_reduced <- td_gamma_reduced %>% 
+  filter(date >= "2007-01-01") %>% 
+  filter(date <= "2010-12-31") 
+
+labels <- tibble(
+  date = c(ymd("2008-09-15")),
+  label_with_this = c("Lehman Brothers"),
+  height = c(0.50)
+)
+
+pms <- tibble(
+  date = c(ymd("2007-12-03", "2010-06-24")),
+  label_with_this = c("Rudd", "Gillard"),
+  height = c(0.60, 0.6)
+)
+
+
+ggplot(data = td_gamma_reduced, 
+       aes(x = date, 
+           y = gamma, 
+           color = as.factor(topic))
+) +
+  geom_jitter(size = 0.5) +
+  geom_smooth() +
+  geom_vline(data = labels, aes(xintercept = date), linetype = "dashed") +
+  geom_vline(data = pms, aes(xintercept = date), linetype = "dotted") +
+  geom_label(data = labels, aes(x = date, y = height, label = label_with_this, color = "black"), color = "black") +
+  geom_label(data = pms, aes(x = date, y = height, label = label_with_this, color = "black"), color = "black") +
   theme_classic() +
   labs(x = "Date",
-       y = "Gamma") +
-  theme(legend.position="none") +
-  # facet_wrap(vars(topic)) +
-  scale_colour_viridis_d() + 
-  theme(text = element_text(size = 20)) +
-  theme(
-    panel.background = element_rect(fill = "transparent") # bg of the panel
-    , plot.background = element_rect(fill = "transparent", color = NA) # bg of the plot
-    , panel.grid.major = element_blank() # get rid of major grid
-    , panel.grid.minor = element_blank() # get rid of minor grid
-    , legend.background = element_rect(fill = "transparent") # get rid of legend bg
-    , legend.box.background = element_rect(fill = "transparent") # get rid of legend panel bg
-  )
+       y = "Proportion") +
+  facet_wrap(vars(chamber), ncol = 2) +
+  # theme(legend.position="none") +
+  scale_colour_viridis_d(name = "Topics")
 
 ggsave(
-  "outputs/figures/topics_example-slides.pdf",
+  "outputs/figures/topics_example-GFC.pdf",
   height = 6,
   width = 8,
   units = "in"
 )
+
